6cf74612c0089a8930561437de15092fceffda50
Beeblebrox <luka.cupic@fer.hr>
2019-10-31 14:42:10 +0100

    Write a simple script that reads a PDF, extracts text, and performs simple preprocessing

---
+++ main.py
@@ -0,0 +1,31 @@
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from tika import parser
+import numpy as np
+import re
+
+ps = PorterStemmer()
+
+def preprocess(text):
+	'''
+	Performs pre-processing of the given text and converts it 
+	into a list of words.
+	'''
+	
+	text = text.lower()
+	words = re.findall(r'\w+', text)
+	
+	stops = stopwords.words('english')
+	filtered = [w for w in words if w not in stops]
+	
+	stemmed = [ps.stem(w) for w in filtered]
+	return stemmed
+
+def main():
+	raw = parser.from_file('astro.pdf')
+	data = raw["content"].lower()
+	print(preprocess(data))
+
+main()
