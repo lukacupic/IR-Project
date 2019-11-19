98267b20100b0478e0847c75c70762207ece7b56
Beeblebrox <luka.cupic@fer.hr>
2019-11-19 12:19:55 +0100

    Add number conversion

--- main.py
+++ main.py
@@ -1,38 +1,54 @@
 import nltk
+
 nltk.download('stopwords')
+nltk.download('wordnet')
+
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 from tika import parser
+from num2words import num2words
 import numpy as np
 import re
 
-numberPattern = re.compile("^[-+]?[0-9]+$")
-singlePattern = re.compile("^[a-z][A-Z][0-9]$")
-
 ps = PorterStemmer()
+lm = WordNetLemmatizer()
+
+letterPattern = re.compile("^[a-z][A-Z]$")
+numberPattern = re.compile("^[-+]?[0-9]+$")
+stops = stopwords.words('english')
 
 def preprocess(text):
 	'''
-	Performs pre-processing of the given text and converts it 
+	Performs pre-processing of the given text and converts it
 	into a list of words.
 	'''
 	
-	text = text.lower()
-	words = re.findall(r'\w+', text)
+	# get all the words
+	words = re.findall(r'\w+', text.lower())
 	
-	stops = stopwords.words('english')
-	filtered = [w for w in words if w not in stops]
-	filtered = [w for w in filtered if not match(w)]
+	# remove stop words
+	words = [w for w in words if w not in stops]
 	
-	stemmed = [ps.stem(w) for w in filtered]
-	return stemmed
+	# remove matched words
+	words = [w for w in words if not match(w)]
+	
+	# convert numbers
+	words = [words2num(w) for w in words if numberPattern.match(w)]
+	
+	# perform stemming
+	words = [ps.stem(w) for w in words]
+	
+	# perform lemmatization
+	words = [lm.lemmatize(w, pos="v") for w in words]
+	
+	return words
 
 def match(word):
-	if numberPattern.match(word):
+	if letterPattern.match(word):
 		return True
-
-	if singlePattern.match(word):
-		return True
+	
+	return False
 
 def main():
 	raw = parser.from_file('dataset/astro.pdf')
