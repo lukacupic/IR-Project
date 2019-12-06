38f71289af3f8a382504be66342f6830f71b4807
Beeblebrox <luka.cupic@fer.hr>
2019-12-06 16:29:11 +0100

    Add tf-idf computation

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 from tika import parser
+
 from num2words import num2words
+from collections import Counter
 import numpy as np
+import math
 import re
 import os
 
@@ -19,11 +22,18 @@ letterPattern = re.compile("^[a-z][A-Z]$")
 numberPattern = re.compile("^[-+]?[0-9]+$")
 stops = stopwords.words('english')
 
-def bitVector(d, mainVector):
+def bitVector(d, corpusVector):
 	bv = []
-	for w in mainVector:
-		bv.append('1' if w in d else '0')
+	for w in corpusVector:
+		bv.append(1 if w in d else 0)
 	return bv
+
+def tfVector(d, corpusVector):
+	tf = []
+	counts = Counter(list(d))
+	for w in corpusVector:
+		tf.append(counts[w])
+	return tf
 
 def preprocess(text):
 	'''
@@ -58,36 +68,50 @@ def match(word):
 	return False
 
 def main():
-	mainVector = set()
+	corpusVector = set()
 	documents = []
 	counter = 0
 	
-	for root, subdirs, files in os.walk('./dataset'):
-		if counter is 5:
-			break
-			
+	for root, subdirs, files in os.walk('./dataset-small'):
 		for filename in files:
-			if counter is 5:
-				break
-			
 			filePath = os.path.join(root, filename)
+			if not filePath.endswith('.txt'):
+				continue
 
 			raw = parser.from_file(filePath)
 			data = raw["content"].lower()
 		
 			words = preprocess(data)
 			
-			mainVector.update(words)
+			corpusVector.update(words)
 			documents.append(words)
-			
-			counter = counter + 1
 	
-	mainList = list(mainVector)
+	tfVectors = []
+	for d in documents:
+		#vector = bitVector(d, corpusVector)
+		vector = tfVector(d, corpusVector)
+		tfVectors.append(vector)
+		#print(vector)
+	#print(corpusVector)
 	
-	#for d in documents:
-		#vector = bitVector(d, mainVector)
+	corpusList = list(corpusVector)
+	idf = []
+	
+	for i in range(len(corpusList)):
+		count = 0
+		for tf in tfVectors:
+			if tf[i] is not 0:
+				count = count + 1
+		idf.append(math.log(len(documents)/count, 10))
+	
+	tfidfs = []
+	npidf = np.array(idf)
+	for tf in tfVectors:
+		tfidfs.append(np.array(tf) * npidf)
 		
-	print(bitVector(documents[0], mainVector))
+	print(corpusVector)
+	for tfidf in tfidfs:
+		print(tfidf)
 
 main()
 
